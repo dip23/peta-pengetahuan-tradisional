ab1859211176790b757368e87eeff653b564c205
difabag <adif.maulana@gmail.com>
2022-06-25 13:49:58 +0700

    remove validation image

--- src/components/fragments/FormBudaya/validation.js
+++ src/components/fragments/FormBudaya/validation.js
@@ -4,17 +4,17 @@ const date = new Date();
 const yearToday = date.getFullYear();
 
 export const formBudayaSchema = yup.object().shape({
-  image: yup.mixed()
-    .required('Foto Kebudayaan wajib diisi')
-    .test({
-      message: "Tidak ada foto yang dipilih",
-      test: arr => arr.length>0
-    })
-    .test(
-      "fileSize", "Ukuran gambar maksimal 2MB", (value)=>{
-        return value[0] && value[0]?.size <= 2*1024*1024;
-      }
-    ),
+  // image: yup.mixed()
+  //   .required('Foto Kebudayaan wajib diisi')
+  //   .test({
+  //     message: "Tidak ada foto yang dipilih",
+  //     test: arr => arr.length>0
+  //   })
+  //   .test(
+  //     "fileSize", "Ukuran gambar maksimal 2MB", (value)=>{
+  //       return value[0] && value[0]?.size <= 2*1024*1024;
+  //     }
+  //   ),
   nama_budaya: yup.string().required('Nama Kebudayaan Wajib diisi'),
   tahun: yup.number().typeError('Tahun Belum Sesuai')
     .min(2010)
