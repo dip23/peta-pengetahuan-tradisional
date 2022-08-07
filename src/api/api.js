c274c19444d45963a4cc962e08c108694fcc7347
difabag <adif.maulana@gmail.com>
2022-08-08 06:33:08 +0700

    add client validation image

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const baseURL = "https://be-pengetahuan-tradisional.herokuapp.com/api/";
+// const baseURL = "http://localhost:5000/api/";
 
 const Api = axios.create({
   baseURL: baseURL,
